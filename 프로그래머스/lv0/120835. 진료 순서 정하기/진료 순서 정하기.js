6282527eed6d6bc38be50b30ab3070c8f7749658
mingyu kim <131242431+kimmingyu9411@users.noreply.github.com>
2023-06-15 21:48:10 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,24 @@
+function solution(emergency) {
+    var answer = [];
+    let clone=[];
+    for(let i=0; i<emergency.length; i++){
+      clone[i]=emergency[i]
+    }
+    for(let i=0; i<clone.length; i++){
+        for(let j=0; j<clone.length; j++){
+            if(clone[i]>clone[j]){
+              let x=clone[i];
+              clone[i]=clone[j];
+              clone[j]=x;
+            }
+        }
+    }
+    for(let i=0; i<clone.length; i++){
+       for(let j=0; j<clone.length; j++){
+           if(emergency[i]==clone[j]){
+              answer[i]=j+1
+           }
+       } 
+    }
+    return answer;
+}
