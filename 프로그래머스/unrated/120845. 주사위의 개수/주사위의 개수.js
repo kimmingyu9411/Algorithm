d6acf9d971e222265844b685dcbbd057272b43ee
mingyu kim <131242431+kimmingyu9411@users.noreply.github.com>
2023-10-18 01:41:05 +0900

    [level unrated] Title: 주사위의 개수, Time: 0.03 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
@@ -1,16 +1,8 @@
 function solution(box, n) {
-    var answer = 0;
-    if(box[0]%n!==0){
-       box[0]-=1;
-    };
-     if(box[1]%n!==0){
-       box[1]-=1; 
-    };
-     if(box[2]%n!==0){
-       box[2]-=1; 
-    };
-   let box_pull=box[0]*box[1]*box[2];
-   let n_pull=n*n*n;
-    answer=box_pull/n_pull
+var answer = 0;
+  let a=Math.floor(box[0]/n)
+  let b=Math.floor(box[1]/n)
+  let c=Math.floor(box[2]/n);
+    answer=a*b*c
     return answer;
 }
