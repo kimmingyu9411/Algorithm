7a05a87b90ad14aa333fc8d3d5588e3555a91e8a
mingyu kim <131242431+kimmingyu9411@users.noreply.github.com>
2023-10-18 23:37:30 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    var answer = 0;
+    for(let i=1; i<=n; i++){
+          let count=0
+        for(let j=1; j<=n; j++){
+            if(i%j==0){
+                count++
+            }
+            if(count>2){
+                answer++
+                break;
+            }
+        }
+    }
+        return answer;
+     }
+
