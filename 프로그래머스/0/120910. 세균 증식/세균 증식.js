f279b3c1a3d6a37680a88404cd53719e952e7a00
mingyu kim <131242431+kimmingyu9411@users.noreply.github.com>
2024-06-10 02:07:44 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,8 +1,8 @@
 function solution(n, t) {
-    var answer = 2;
-    for(let i=1; i<t; i++){
+    var answer = n;
+    for(let i=0; i<t; i++){
         answer=answer*2
     }
-    answer=answer*n
+   
     return answer;
 }
